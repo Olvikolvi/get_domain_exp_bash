32f7e21302e79b5c23c553a279242cbf37f0f49c
Mikko Lampikoski <mikko.lampikoski@doop.fi>
2020-08-19 11:59:28 +0300

    make command quiet

--- check.sh
+++ check.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+#list="list.txt"
+if [ ! -z $1 ]
+then
+list=$1
+fi
 for domain in $(cat list.txt)
 do
   domain2=$domain
-  if command -v idn; then domain2=$(echo $domain | idn); fi
+  if command -v idn > /dev/null; then domain2=$(echo $domain | idn); fi
   exp=$(whois ${domain2} | grep -i expi | head -n 1 | egrep -o '[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}|[0-9]{4}\-[0-9]{2}\-[0-9]{2}')
   expf=$(date -j -f '%d.%m.%Y' "${exp}" '+%d.%m.%Y' 2> /dev/null)
   if [ -z "$expf" ]
